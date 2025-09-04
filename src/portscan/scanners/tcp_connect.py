1e39298f66b589c56bbe75ff40ee4aa4d9fca743
Filip Nowacki <nowacki.filip.m@gmail.com>
2025-09-04 19:53:42 +0200

    TCPConnectScanner Object Class & Basic Doc

--- src/portscan/scanners/tcp_connect.py
+++ src/portscan/scanners/tcp_connect.py
@@ -3,6 +3,7 @@ from contextlib import suppress
 from dataclasses import dataclass
 from typing import Optional
 
+#Data Container with all the info about a single port probe
 @dataclass
 class ProbeResult: 
     host: str
@@ -12,4 +13,11 @@ class ProbeResult:
     banner: Optional[str] = None 
     reason: Optional[str] = None
 
+#Scanner Object 
+class TCPConnectScanner: 
+
+    def __init__(self, timeout: float = 0.8, banner_timeout = 0.1, banner_bytes: int = 128):
+        self.timeout = timeout
+        self.banner_timout = banner_timeout
+        self.banner_bytes = banner_bytes
 
